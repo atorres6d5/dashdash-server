6fb2ed145d6356e254dcbbe271267b4d25f32181
Kevin Springer <KevinUSCU@outlook.com>
2017-12-20 09:14:13 -0800

    Add drop file code.

--- db/seeds/00_drop.js
+++ db/seeds/00_drop.js
@@ -1,13 +1,10 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex('reality').del()
+    .then(() => knex('items').del())
+    .then(() => knex('plans').del())
+    .then(() => knex('badges').del())
+    .then(() => knex('users').del())
+    .then(() => knex('template_items').del())
+    .then(() => knex('template_plans').del())
+}
