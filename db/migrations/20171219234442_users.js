79261824f07c1d2208f1057bbec4b8f05623896d
Kevin Springer <KevinUSCU@outlook.com>
2017-12-20 00:11:54 -0800

    Build users table and seeds.

--- db/migrations/20171219234442_users.js
+++ db/migrations/20171219234442_users.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+  return knex.schema.createTable('users', (table) => {
+    table.increments()
+    table.string('first_name').notNullable().defaultTo('')
+    table.string('last_name').notNullable().defaultTo('')
+    table.string('username').notNullable().defaultTo('')
+    table.string('hashed_password').notNullable().defaultTo('')
+    table.boolean('children').notNullable().defaultTo(false)
+    table.boolean('pets').notNullable().defaultTo(false)
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTable('users')
+}
--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  // Inserts sample users
+  // Plain text password for all sample users is 'password'
+  return knex('users').insert([
+    { id: 1, first_name: 'Rando', last_name: 'Ipsum', username: 'babbles', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa', children: false, pets: false },
+    { id: 2, first_name: 'Theon', last_name: 'Greyjoy', username: 'reek', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa', children: false, pets: true },
+    { id: 3, first_name: 'Mother', last_name: 'Hubbard', username: 'goose', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa', children: true, pets: true },
+    { id: 4, first_name: 'Flash', last_name: 'Mulligan', username: 'zippy', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa', children: true, pets: false },
+  ])
+  .then(() => {
+    return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`)
+  })
+}
