f6d06904ad8d390856b384842bb7c4a523d79e3d
Kevin Springer <KevinUSCU@outlook.com>
2017-12-23 00:37:35 -0800

    Add additional error types.

--- models/error.model.js
+++ models/error.model.js
@@ -2,9 +2,18 @@ function processErrorMessage(err) {
   /* This selectively prints errors to the server and client. In the default case, where the error comes from a function such as knex, etc. where sending details to the client may be a security concern, we send a generic message to the client while printing the details on the server. This process also helps prevent the continued execution of chained promises after an error is thrown. */
   if (err.message) {
     switch (err.message) {
+      case 'duplicateUser': return { status: 409, message: 'A user with this email address already exists' }
       case 'invalidPassword': return { status: 401, message: 'Incorrect password' }
       case 'invalidToken': return { status: 401, message: 'A valid authorization token is required' }
       case 'missingPassword': return { status: 400, message: 'A password is required' }
+      case 'missingUsername': return { status: 400, message: 'A username is required' }
+      case 'noSuchUser': return { status: 404, message: 'This user does not exist' }
+      case 'noSuchTemplate': return { status: 404, message: 'This template does not exist' }
+      case 'noSuchPlan': return { status: 404, message: 'This plan does not exist' }
+      case 'noSuchItem': return { status: 404, message: 'This item does not exist' }
+      case 'noSuchBadge': return { status: 404, message: 'This badge does not exist' }
+      case 'requestorInvalid': return { status: 401, message: 'Requestor is not a valid user' }
+      case 'unauthorizedUser': return { status: 401, message: 'User is not authorized to access this resource' }
       default:
         console.log(err)
         return { status: 500, message: 'Our apologies, but an internal server error has occurred' }
