5c5a3fd5b2c7905ad3c1a35a00a8b4da6023439d
Kevin Springer <KevinUSCU@outlook.com>
2017-12-23 00:38:50 -0800

    Add auth routes for signup and login.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,9 @@
-const router = require('express').Router()
-const auth = require(`../controllers/auth.controller.js`)
+const express = require('express')
+const router = express.Router()
+const { AuthController } = require(`../controllers`)
 
-
+// Authentication
+router.post('/signup', AuthController.signup) // new users should only be created through signup
+router.post('/login', AuthController.login)
 
 module.exports = router
