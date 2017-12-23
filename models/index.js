95579ffc389dd07373fb194a1993a17445a81d71
Kevin Springer <KevinUSCU@outlook.com>
2017-12-23 00:37:47 -0800

    Add TokenModel.

--- models/index.js
+++ models/index.js
@@ -3,5 +3,6 @@ module.exports = {
     TemplateModel: require('./templates.model.js'),
     BadgesModel: require('./badges.model.js'),
     AuthModel: require('./auth.model.js'),
+    TokenModel: require('./token.model'),
     ErrorModel: require('./error.model.js')
 }
