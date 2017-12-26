5d1c77aac77e6e20f9d8e425e1af9a57609dcea0
Kevin Springer <KevinUSCU@outlook.com>
2017-12-26 13:09:22 -0800

    Simplify auth track to verify user/admin. Specific user matching will be handled by individual requests.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const router = express.Router()
-const { UsersController } = require('../controllers')
+const { AuthController, UsersController } = require('../controllers')
 
 // Creating a user should be done though auth/signup route
 
 // Edit user
 // Change user profile
-router.put('/:id', AuthController.isOwnerOfUser, UsersController.update)
+router.put('/:id', AuthController.isUser, UsersController.update)
 // Change user role
 router.put('/promote/:id', AuthController.isAdmin, UsersController.changeRole)
 
 // View users
-// View user by token
+// View user by token (this gets the current user)
 router.get('/fromToken', UsersController.showOneFromToken)
-// View all users
+// View all users (admin function)
 router.get('/', AuthController.isAdmin, UsersController.index)
-// View user by id route (might not be needed?)
-router.get('/:id', AuthController.isOwnerOfUserOrAdmin, UsersController.showOne)
+// View user by id route (admin function)
+router.get('/:id', AuthController.isAdmin, UsersController.show)
 
-// Delete User
-router.delete('/:id', AuthController.isOwnerOfUserOrAdmin, UsersController.destroy)
+// Delete User (note, admin accounts cannot be deleted; they must be demoted first)
+router.delete('/:id', AuthController.isAdmin, UsersController.destroy)
 
 module.exports = router
