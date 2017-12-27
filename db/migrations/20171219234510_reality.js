122199ab4d4fedbf689b7c4736d703a3419ae9a4
Kevin Springer <KevinUSCU@outlook.com>
2017-12-27 12:13:52 -0800

    Add migration table.

--- db/migrations/20171219234510_reality.js
+++ db/migrations/20171219234510_reality.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+  return knex.schema.createTable('reality', (table) => {
+    table.increments()
+    table.integer('item_id').notNullable()
+    table.foreign('item_id').references('items.id').onDelete('CASCADE')
+    table.time('start_time').defaultTo(null)
+    table.time('actual_duration').defaultTo(null)
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTable('reality')
+}
