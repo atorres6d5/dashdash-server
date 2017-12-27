608663cb1c5de42f0fd0d43172051612180a32ff
Kevin Springer <KevinUSCU@outlook.com>
2017-12-26 16:13:18 -0800

    Reorganize file order for legibility.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,15 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { AuthController, UsersController } = require('../controllers')
 
-// Creating a user should be done though auth/signup route
+// CREATE USER
+// Should be done though auth/signup route
 
-// Edit user
-// Change user profile
-router.put('/:id', AuthController.isUser, UsersController.update)
-// Change user role
-router.put('/promote/:id', AuthController.isAdmin, UsersController.changeRole)
-
-// View users
+// VIEW USERS
 // View user by token (this gets the current user)
 router.get('/fromToken', UsersController.showOneFromToken)
 // View all users (admin function)
@@ -18,7 +13,14 @@ router.get('/', AuthController.isAdmin, UsersController.index)
 // View user by id route (admin function)
 router.get('/:id', AuthController.isAdmin, UsersController.show)
 
-// Delete User (note, admin accounts cannot be deleted; they must be demoted first)
+// EDIT USER
+// Change user profile
+router.put('/:id', AuthController.isUser, UsersController.update)
+// Change user role
+router.put('/promote/:id', AuthController.isAdmin, UsersController.changeRole)
+
+// DELETE USER
+// (note, admin accounts cannot be deleted; they must be demoted first)
 router.delete('/:id', AuthController.isAdmin, UsersController.destroy)
 
 module.exports = router
