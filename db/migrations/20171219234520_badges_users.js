1be0af6ca5f542d26562fde6d6cef480d260932b
Kevin Springer <KevinUSCU@outlook.com>
2017-12-27 12:15:06 -0800

    Add 'unlocked'. Make 'badge_id' and 'user_id' foreign keys.

--- db/migrations/20171219234520_badges_users.js
+++ db/migrations/20171219234520_badges_users.js
@@ -1,8 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('badges_users', (table) => {
     table.increments()
-    table.integer('badge_id').notNullable().defaultTo('')
-    table.integer('user_id').notNullable().defaultTo('')
+    table.integer('badge_id').notNullable()
+    table.foreign('badge_id').references('badges.id').onDelete('CASCADE')
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('users.id').onDelete('CASCADE')
+    table.boolean('unlocked').notNullable().defaultTo(false)
     table.timestamps(true, true)
   })
 };
