0c889e268697ba3c49e09205b8b7953c121dcb9b
Kevin Springer <KevinUSCU@outlook.com>
2017-12-27 10:38:30 -0800

    Remove unneeded join tables.

--- db/seeds/09_template_plans_template_items.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex, Promise) {
-  return knex('template_plan_templates_item').insert([
-    { }
-  ])
-  .then(() => {
-    return knex.raw(`SELECT setval('template_plans_template_items_id_seq', (SELECT MAX(id) FROM template_plans_template_items));`)
-  })
-}
