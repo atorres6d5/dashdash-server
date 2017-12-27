5caccd380b43116e6856a001cd7556432aebcf76
Kevin Springer <KevinUSCU@outlook.com>
2017-12-26 16:32:50 -0800

    Remove create and defer to default Model create method.

--- models/user.model.js
+++ models/user.model.js
@@ -3,21 +3,12 @@ const db = require('../db/knex')
 const bcrypt = require('bcryptjs')
 
 class UserModel extends Model {
+  
   static getUserIdByUsername (username) {
     return db('users')
     .select('id')
     .where({ username })
     .first()
-  }
-
-  
-
-  static create (first_name, last_name, email, password) { 
-    const hashed_password = bcrypt.hashSync(password)
-    return db('users')
-    .insert({ first_name, last_name, email, hashed_password })
-    // note that 'role' is automatically defaulted to 'user' by the db
-    .returning(['id'])
   }
   
 }
