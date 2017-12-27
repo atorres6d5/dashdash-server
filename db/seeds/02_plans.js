ba1384c7651c8d86e55ecf7a2ef24d9e0b3b9fa6
Kevin Springer <KevinUSCU@outlook.com>
2017-12-27 13:33:20 -0800

    Fix typo.

--- db/seeds/02_plans.js
+++ db/seeds/02_plans.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   return knex('plans').insert([
-    { id: 1, user_id: 1, name: 'Bespin Wakeup', active: true, successes: 2, runs: 3, start_time: '08:00:00', days: 'MON,TUE,WED,THU,FRI', parent_plan: null },
-    { id: 2, user_id: 1, name: 'Holocron Practice', active: true, successes: 0, runs: 4, start_time: '09:00:00', days: 'SUN,SAT', parent_plan: null }
+    { id: 1, user_id: 1, name: 'Bespin Wakeup', active: true, successes: 2, runs: 3, start_time: '08:00:00', days_to_run: 'MON,TUE,WED,THU,FRI', parent_plan: null },
+    { id: 2, user_id: 1, name: 'Holocron Practice', active: true, successes: 0, runs: 4, start_time: '09:00:00', days_to_run: 'SUN,SAT', parent_plan: null }
   ])
   .then(() => {
     return knex.raw(`SELECT setval('plans_id_seq', (SELECT MAX(id) FROM plans));`)
