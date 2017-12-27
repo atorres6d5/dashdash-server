48b9b7840507d053c1ee16cd7fd97b75d9a312ae
Kevin Springer <KevinUSCU@outlook.com>
2017-12-27 12:16:59 -0800

    Add foreign key 'template_plan_id'. Remove 'description'. Add 'order'. Rename 'common-duration' to 'duration' and 'skippable' to 'snoozable'.

--- db/migrations/20171219234537_template_items.js
+++ db/migrations/20171219234537_template_items.js
@@ -2,10 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('template_items', (table) => {
     table.increments()
+    table.integer('template_plan_id').notNullable()
+    table.foreign('template_plan_id').references('template_plans.id').onDelete('CASCADE')
     table.string('name').notNullable().defaultTo('')
-    table.string('description').notNullable().defaultTo('')
-    table.integer('common-duration').notNullable().defaultTo('')
-    table.boolean('skippable').notNullable().defaultTo('')
+    table.time('duration').notNullable().defaultTo('00:05:00')
+    table.boolean('snoozable').notNullable().defaultTo(false)
+    table.integer('order').unique().defaultTo(null)
     table.timestamps(true, true)
   })
 };
