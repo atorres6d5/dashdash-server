d87c5be68581b502c8211c5dc88ffdfe1ae17ceb
Kevin Springer <KevinUSCU@outlook.com>
2017-12-26 16:11:18 -0800

    Fix showOneFromToken, update, changeRole and destroy.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,54 +6,48 @@ class UsersController extends Controller {
 
   static showOneFromToken (req, res, next) {
     // Validate and decode token
-    Token.verifyAndExtractHeaderToken(req.headers)
+    TokenModel.verifyAndExtractHeaderToken(req.headers)
     .catch(err => { throw new Error('invalidToken') })
     // Check for and retrieve user from database
     .then(token => UserModel.find(token.sub.id))
-    .then(response => {
-      if (!response) throw new Error('noSuchUser')
-      return res.status(200).json({ user: response })
+    .then(result => {
+      if (!result) throw new Error('noSuchUser')
+      return res.status(200).json({ user: result })
     })
     .catch(next)
   }
 
   static update (req, res, next) {
     // *** Editing profile data can only be done by the user whose id we are updating ***
-    const id = req.params.id
-    const { first_name, last_name, email, password } = req.body
-    // If email was changed, verify no duplicates
-    if (email) {
-      UserModel.getUserIdByEmail(email)
-      .then(existingUser => {
-        if (existingUser) throw new Error('duplicateUser')
-      })
-      // Update user profile with supplied data
-      .then(() => UserModel.update(id, first_name, last_name, email, password))
-      .then(userId => res.status(200).json({ response: userId }))
-      .catch(next)
-    } else {
-      // Update user profile with supplied data
-      UserModel.update(id, first_name, last_name, undefined, password)
-      .then(userId => res.status(200).json({ response: userId }))
-      .catch(next)
-    }
-  }
-
-  static destroy (req, res, next) {
-    // *** Deleting a user requires either role of 'admin' or the user who's id we are requesting ***
-    // Admin accounts cannot be deleted
-    const id = req.params.id
-    UserModel.getUser(id)
-    .then(response => {
-      if (response.role === 'admin') throw new Error('cannotDeleteAdmin')
+    // Role and password cannot be updated in this way
+    const id = Number(req.params.id)
+    const { username, first_name, last_name, children, pets } = req.body
+    // Note we are stripping out any non-changeable fields
+    const updatedUser = { username, first_name, last_name, children, pets }
+    // Verify user owns profile being updated by checking route against header token
+    TokenModel.verifyAndExtractHeaderToken(req.headers)
+    .catch(err => { throw new Error('invalidToken') })
+    .then(token => {
+      if (token.sub.id !== id) throw new Error('unauthorizedUser')
+      // If username was changed, verify no duplicates
+      if (username) {
+        UserModel.getUserIdByUsername(username)
+        .then(existingUser => {
+          if (existingUser) throw new Error('duplicateUser')
+        })
+        // Update user profile with supplied data
+        .then(() => UserModel.update(id, updatedUser))
+        .then(result => res.status(200).json({ user: result }))
+        .catch(next)
+      } else {
+        // Update user profile with supplied data
+        UserModel.update(id, updatedUser)
+        .then(result => res.status(200).json({ user: result }))
+        .catch(next)
+      }
     })
-    // Delete user
-    .then(() => UserModel.destroy(id))
-    .then(response => res.status(204).json())
     .catch(next)
   }
-
-  
 
   static changeRole (req, res, next) {
     // An 'admin' token is required to change the role of another user
@@ -62,13 +56,28 @@ class UsersController extends Controller {
     if (!role) throw new Error('missingRole')
     if (role !== 'admin' && role !== 'user') throw new Error('incorrectRoleType')
     // Update role of user in db
-    UserModel.update(id, undefined, undefined, undefined, undefined, role)
-    .then(userId => {
-      if (!userId) throw new Error('noSuchUser')
-      return res.status(200).json({ response: userId })
+    UserModel.update(id, { role })
+    .then(result => {
+      if (!result) throw new Error('noSuchUser')
+      return res.status(200).json({ user: result })
     })
     .catch(next)
   }
+
+  static destroy (req, res, next) {
+    // *** Deleting a user requires role of 'admin' ***
+    // Admin accounts cannot be deleted
+    const id = req.params.id
+    UserModel.find(id)
+    .then(result => {
+      if (result.role === 'admin') throw new Error('cannotDeleteAdmin')
+    })
+    // Delete user
+    .then(() => UserModel.destroy(id))
+    .then(result => res.status(204).json())
+    .catch(next)
+  }
+
 }
 
 module.exports = UsersController
