8d0881a022105bb4c5213705ba79efff9647cca6
Aldo Torres <atorres6d5@gmail.com>
2017-12-28 11:23:40 -0800

    fixed img to image

--- db/seeds/05_badges.js
+++ db/seeds/05_badges.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     { id: 3, name: 'Legend', description: 'Creating 4 schedules', image: './img/badges/legend.svg' },
     { id: 4, name: 'Beastmaster', description: 'Including pets in a schedule', image: './img/badges/beastmaster.svg' },
     { id: 5, name: 'Punctuality Wizard', description: 'Completing five schedules in a row without missing a day', image: './img/badges/wizard.svg' },
-    {id: 6, name: 'Skipper', description: 'Skipping more than one task', img: './img/badges/skipper.svg'},
-    {id: 7, name: 'Late', description: 'Arrived late', img:'./img/badges/late.svg'}
+    {id: 6, name: 'Skipper', description: 'Skipping more than one task', image: './img/badges/skipper.svg'},
+    {id: 7, name: 'Late', description: 'Arrived late', image:'./img/badges/late.svg'}
   ])
   .then(() => {
     return knex.raw(`SELECT setval('badges_id_seq', (SELECT MAX(id) FROM badges));`)
