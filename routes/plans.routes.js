41226f94b2078398639a941f2f8267d27d5bead5
Kevin Springer <KevinUSCU@outlook.com>
2017-12-28 14:39:45 -0800

    Add distinctions between getting plans for a user and admin ability to view all plans.

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -1,10 +1,26 @@
 const router = require('express').Router()
 const { AuthController, PlansController } = require('../controllers')
 
+// Get *ALL* plans; this is Admin only
 router.get('/', AuthController.isAdmin, PlansController.index)
-router.get('/:id', AuthController.ownsThisPlan, PlansController.show)
-router.post('/', AuthController.ownsThisPlan, PlansController.create)
-router.put('/:id', AuthController.ownsThisPlan, PlansController.update)
+
+// Get a specific plan; this is admin only
+router.get('/:id', AuthController.isAdmin, PlansController.show)
+
+// Get all plans for user (token must match user)
+router.get('/user/:userId', AuthController.isThisUser, PlansController.index)
+
+// Get a specific plan for a user (token must match user)
+router.get('/:id/user/:userId', AuthController.isThisUser, PlansController.show)
+
+// Create plan for a user (token must match user)
+router.post('/user/:userId', AuthController.isThisUser, PlansController.create)
+
+// Update plan for a user (token must match user)
+router.put('/:id/user/:userId', AuthController.isThisUser, PlansController.update)
+
+// Delete plan (for the current user token)
 router.delete('/:id', AuthController.ownsThisPlan, PlansController.destroy)
+// router.delete('/:id/user/:userId', AuthController.isThisUser, PlansController.destroy)
 
 module.exports = router
