e4dfd41369d63a3ef9269e3e82694af1bbd97487
Kevin Springer <KevinUSCU@outlook.com>
2018-01-03 10:53:16 -0800

    Fill in missing templates and remove unused ones.

--- db/seeds/07_template_plans.js
+++ db/seeds/07_template_plans.js
@@ -2,32 +2,44 @@ exports.seed = function(knex, Promise) {
   return knex('template_plans').insert([
     {
       id: 1,
-      name: 'Urban Dweller',
-      description: 'Creating your first schedule'
+      name: 'Weekday With Kids and Pets',
+      description: 'Weekday schedule for someone who has pets and kids',
+      type: 'weekday'
     }, {
       id: 2,
-      name: 'Weekday With Kids and Pets',
-      description: 'Someone who has pets and kids weekday schedule'
+      name: 'Weekday With Kids',
+      description: 'Weekday schedule for someone who has kids',
+      type: 'weekday'
     }, {
       id: 3,
-      name: 'Weekday With kids',
-      description: 'Someone who has kids weekday schedule'
+      name: 'Weekday With Pets',
+      description: 'Weekday schedule for someone who has pets',
+      type: 'weekday'
     }, {
       id: 4,
       name: 'Weekday Schedule',
-      description: 'Someone who is with out dependents weekday schedule'
+      description: 'Weekday schedule for someone who is without dependents',
+      type: 'weekday'
     }, {
       id: 5,
       name: 'Weekend With Kids and Pets',
-      description: 'Weekend for someone with pets and kids and wants to watch morning cartoons'
+      description: 'Weekend schedule for someone who has pets and kids and watches cartoons',
+      type: 'weekend'
     }, {
       id: 6,
       name: 'Weekend With Kids',
-      description: 'Weekend With kids and watch cartoons'
+      description: 'Weekend schedule for someone who has kids and watches cartoons',
+      type: 'weekend'
     }, {
       id: 7,
+      name: 'Weekend With Pets',
+      description: 'Weekend schedule for someone who has pets and watches cartoons',
+      type: 'weekend'
+    }, {
+      id: 8,
       name: 'Weekend Schedule',
-      description: 'Weekend for someone with no dependents'
+      description: 'Weekend schedule for someone who has no dependents and watches cartoons',
+      type: 'weekend'
     }
   ]).then(() => {
     return knex.raw(`SELECT setval('template_plans_id_seq', (SELECT MAX(id) FROM template_plans));`)
--- db/seeds/08_template_items.js
+++ db/seeds/08_template_items.js
@@ -4,327 +4,404 @@ exports.seed = function(knex, Promise) {
       id: 1,
       template_plan_id: 1,
       name: 'Shower',
-      duration: 8,
-      skippable: true,
-      order: 1
-    }, {
-      id: 2,
-      template_plan_id: 2,
-      name: 'Shower',
       duration: 10,
       skippable: true,
       order: null
     }, {
-      id: 3,
-      template_plan_id: 2,
+      id: 2,
+      template_plan_id: 1,
       name: 'Wake up Kids',
+      duration: 5,
+      skippable: false,
+      order: null
+    }, {
+      id: 3,
+      template_plan_id: 1,
+      name: 'Get Self Dressed',
       duration: 5,
       skippable: false,
       order: null
     }, {
       id: 4,
-      template_plan_id: 2,
-      name: 'Get Self Dressed',
+      template_plan_id: 1,
+      name: 'Get Kids Dressed',
       duration: 5,
       skippable: false,
       order: null
     }, {
       id: 5,
+      template_plan_id: 1,
+      name: 'Check kids homework',
+      duration: 10,
+      skippable: true,
+      order: null
+    }, {
+      id: 6,
+      template_plan_id: 1,
+      name: 'Walk Pet/Pet Potty',
+      duration: 6,
+      skippable: false,
+      order: null
+    }, {
+      id: 7,
+      template_plan_id: 1,
+      name: 'Feed Kids',
+      duration: 8,
+      skippable: false,
+      order: null
+    }, {
+      id: 8,
+      template_plan_id: 1,
+      name: 'Feed Pets',
+      duration: 2,
+      skippable: false,
+      order: null
+    }, {
+      id: 9,
+      template_plan_id: 1,
+      name: 'Eat',
+      duration: 8,
+      skippable: true,
+      order: null
+    }, {
+      id: 10,
+      template_plan_id: 1,
+      name: 'Pack Lunch',
+      duration: 5,
+      skippable: true,
+      order: null
+    }, {
+      id: 11,
+      template_plan_id: 1,
+      name: 'Pack Kids Lunch',
+      duration: 5,
+      skippable: true,
+      order: null
+    }, {
+      id: 12,
+      template_plan_id: 1,
+      name: 'Lock Home',
+      duration: 3,
+      skippable: false,
+      order: null
+    }, {
+      id: 13,
+      template_plan_id: 1,
+      name: 'Get Kids to School/Bus Stop',
+      duration: 8,
+      skippable: false,
+      order: null
+    }, {
+      id: 14,
+      template_plan_id: 1,
+      name: 'Pet Day Care',
+      duration: 8,
+      skippable: true,
+      order: null
+    }, {
+      id: 15,
+      template_plan_id: 1,
+      name: 'Get to destination',
+      duration: 8,
+      skippable: false,
+      order: null
+    }, {
+      id: 16,
+      template_plan_id: 2,
+      name: 'Shower',
+      duration: 10,
+      skippable: true,
+      order: null
+    }, {
+      id: 17,
+      template_plan_id: 2,
+      name: 'Wake up Kids',
+      duration: 5,
+      skippable: false,
+      order: null
+    }, {
+      id: 18,
+      template_plan_id: 2,
+      name: 'Get Self Dressed',
+      duration: 5,
+      skippable: false,
+      order: null
+    }, {
+      id: 19,
       template_plan_id: 2,
       name: 'Get Kids Dressed',
       duration: 5,
       skippable: false,
       order: null
     }, {
-      id: 6,
+      id: 20,
       template_plan_id: 2,
       name: 'Check kids homework',
       duration: 10,
       skippable: true,
       order: null
     }, {
-      id: 7,
-      template_plan_id: 2,
-      name: 'Walk Pet/Pet Potty',
-      duration: 6,
-      skippable: false,
-      order: null
-    }, {
-      id: 8,
+      id: 21,
       template_plan_id: 2,
       name: 'Feed Kids',
       duration: 8,
       skippable: false,
       order: null
     }, {
-      id: 9,
-      template_plan_id: 2,
-      name: 'Feed Pets',
-      duration: 2,
-      skippable: false,
-      order: null
-    }, {
-      id: 10,
+      id: 22,
       template_plan_id: 2,
       name: 'Eat',
       duration: 8,
       skippable: true,
       order: null
     }, {
-      id: 11,
+      id: 23,
+      template_plan_id: 2,
+      name: 'Get Dressed',
+      duration: 8,
+      skippable: false,
+      order: null
+    }, {
+      id: 24,
+      template_plan_id: 2,
+      name: 'Get Kids Dressed',
+      duration: 10,
+      skippable: false,
+      order: null
+    }, {
+      id: 25,
       template_plan_id: 2,
       name: 'Pack Lunch',
       duration: 5,
       skippable: true,
       order: null
     }, {
-      id: 12,
+      id: 26,
       template_plan_id: 2,
       name: 'Pack Kids Lunch',
       duration: 5,
       skippable: true,
       order: null
     }, {
-      id: 13,
+      id: 27,
       template_plan_id: 2,
       name: 'Lock Home',
       duration: 3,
       skippable: false,
       order: null
     }, {
-      id: 14,
+      id: 28,
       template_plan_id: 2,
       name: 'Get Kids to School/Bus Stop',
-      duration: 8,
+      duration: 10,
       skippable: false,
       order: null
     }, {
-      id: 15,
-      template_plan_id: 2,
-      name: 'Pet Day Care',
-      duration: 8,
-      skippable: true,
-      order: null
-    }, {
-      id: 16,
+      id: 29,
       template_plan_id: 2,
       name: 'Get to destination',
       duration: 8,
       skippable: false,
       order: null
     }, {
-      id: 17,
+      id: 30,
       template_plan_id: 3,
       name: 'Shower',
       duration: 10,
       skippable: true,
       order: null
     }, {
-      id: 18,
-      template_plan_id: 3,
-      name: 'Wake up Kids',
-      duration: 5,
-      skippable: false,
-      order: null
-    }, {
-      id: 19,
+      id: 31,
       template_plan_id: 3,
       name: 'Get Self Dressed',
       duration: 5,
       skippable: false,
       order: null
     }, {
-      id: 20,
+      id: 32,
       template_plan_id: 3,
-      name: 'Get Kids Dressed',
-      duration: 5,
+      name: 'Walk Pet/Pet Potty',
+      duration: 6,
       skippable: false,
       order: null
     }, {
-      id: 21,
+      id: 33,
       template_plan_id: 3,
-      name: 'Check kids homework',
-      duration: 10,
-      skippable: true,
-      order: null
-    }, {
-      id: 22,
-      template_plan_id: 3,
-      name: 'Feed Kids',
-      duration: 8,
+      name: 'Feed Pets',
+      duration: 2,
       skippable: false,
       order: null
     }, {
-      id: 23,
+      id: 34,
       template_plan_id: 3,
       name: 'Eat',
       duration: 8,
       skippable: true,
       order: null
     }, {
-      id: 24,
-      template_plan_id: 3,
-      name: 'Get Dressed',
-      duration: 8,
-      skippable: false,
-      order: null
-    }, {
-      id: 25,
-      template_plan_id: 3,
-      name: 'Get Kids Dressed',
-      duration: 10,
-      skippable: false,
-      order: null
-    }, {
-      id: 26,
+      id: 35,
       template_plan_id: 3,
       name: 'Pack Lunch',
       duration: 5,
       skippable: true,
       order: null
     }, {
-      id: 27,
-      template_plan_id: 3,
-      name: 'Pack Kids Lunch',
-      duration: 5,
-      skippable: true,
-      order: null
-    }, {
-      id: 28,
+      id: 36,
       template_plan_id: 3,
       name: 'Lock Home',
       duration: 3,
       skippable: false,
       order: null
     }, {
-      id: 29,
+      id: 37,
       template_plan_id: 3,
-      name: 'Get Kids to School/Bus Stop',
-      duration: 10,
-      skippable: false,
+      name: 'Pet Day Care',
+      duration: 8,
+      skippable: true,
       order: null
     }, {
-      id: 30,
+      id: 38,
       template_plan_id: 3,
       name: 'Get to destination',
       duration: 8,
       skippable: false,
       order: null
     }, {
-      id: 31,
+      id: 39,
       template_plan_id: 4,
       name: 'Shower',
       duration: 10,
       skippable: true,
       order: null
     }, {
-      id: 32,
+      id: 40,
       template_plan_id: 4,
       name: 'Eat',
       duration: 8,
       skippable: true,
       order: null
     }, {
-      id: 33,
+      id: 41,
       template_plan_id: 4,
       name: 'Get Dressed',
       duration: 8,
       skippable: false,
       order: null
     }, {
-      id: 34,
+      id: 42,
       template_plan_id: 4,
       name: 'Pack Lunch',
       duration: 5,
       skippable: true,
       order: null
     }, {
-      id: 35,
+      id: 43,
       template_plan_id: 4,
       name: 'Lock Home',
       duration: 3,
       skippable: false,
       order: null
     }, {
-      id: 36,
+      id: 44,
       template_plan_id: 4,
       name: 'Get to destination',
       duration: 8,
       skippable: false,
       order: null
     },{
-      id: 37,
+      id: 45,
       template_plan_id: 5,
       name: 'Feed pets',
       duration: 3,
       skippable: false,
       order: null
     },{
-      id: 38,
+      id: 46,
       template_plan_id: 5,
       name: 'Feed Kids',
       duration: 15,
       skippable: false,
       order: null
     },{
-      id: 39,
+      id: 47,
       template_plan_id: 5,
       name: 'Make Coffee',
       duration: 5,
       skippable: true,
       order: null
     },{
-      id: 40,
+      id: 48,
       template_plan_id: 5,
       name: 'Watch Cartoons',
       duration: 60,
       skippable: false,
       order: null
     },{
-      id: 41,
+      id: 49,
       template_plan_id: 6,
       name: 'Feed pets',
       duration: 3,
       skippable: false,
       order: null
     },{
-      id: 42,
+      id: 50,
       template_plan_id: 6,
       name: 'Feed kids',
       duration: 15,
       skippable: false,
       order: null
     },{
-      id: 43,
+      id: 51,
       template_plan_id: 6,
       name: 'Make Coffee',
       duration: 5,
       skippable: true,
       order: null
     },{
-      id: 44,
+      id: 52,
       template_plan_id: 6,
       name: 'Watch Cartoons',
       duration: 30,
       skippable: false,
       order: null
     },{
-      id: 45,
+      id: 53,
       template_plan_id: 7,
+      name: 'Feed pets',
+      duration: 3,
+      skippable: false,
+      order: null
+    },{
+      id: 54,
+      template_plan_id: 7,
+      name: 'Make Coffee',
+      duration: 5,
+      skippable: true,
+      order: null
+    },{
+      id: 55,
+      template_plan_id: 7,
+      name: 'Watch Cartoons',
+      duration: 60,
+      skippable: false,
+      order: null
+    },{
+      id: 56,
+      template_plan_id: 8,
       name: 'Take a Shower',
       duration: 10,
       skippable: true,
       order: null
     },{
-      id: 46,
-      template_plan_id: 7,
+      id: 57,
+      template_plan_id: 8,
       name: 'Make Coffee',
       duration: 3,
       skippable: true,
       order: null
     },{
-      id: 47,
-      template_plan_id: 7,
+      id: 58,
+      template_plan_id: 8,
       name: 'Do whatever you want',
       duration: 3,
       skippable: false,
